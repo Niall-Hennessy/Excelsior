82ba33e4efa11e6448361b5d4778a635b96b8f63
ChloeK <chloe.keeley@ucdconnect.ie>
2021-03-16 16:05:48 +0000

    working on bottom panel

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,6 +13,11 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Button;
+
 
 public class Main extends Application {
 
@@ -37,12 +44,39 @@ public class Main extends Application {
         menuBox.setMinWidth(960);
         menuBox.setStyle("-fx-background-color: gray");
 
+        Pane bottomPane = new Pane();
+        bottomPane.getChildren().add(new Label("Test"));
+        bottomPane.setStyle("-fx-background-color: green");
+        bottomPane.setMinSize(500, 100);
+
+        Button colourHair = new Button("Colour Hair");
+        Button colourBody = new Button("Colour Body");
+        //HBox hbox = new HBox(20, colourBody, colourHair);
+        //hbox.setSpacing(50);
+
+        HBox bottomBox = new HBox(bottomPane, colourHair, colourBody);
+        bottomBox.setAlignment(Pos.BOTTOM_LEFT);
+        bottomBox.setStyle("-fx-background-color: red");
+        bottomBox.setMargin(bottomPane, new Insets(10, 10, 10, 10));
+        bottomBox.setMargin(colourBody, new Insets(10, 10, 10, 10));
+        bottomBox.setMargin(colourHair, new Insets(10, 10, 10, 10));
+
+        /*
+        CheckBox colourBox = new CheckBox("Hair");
+        HBox colourCheckBox = new HBox(colourBox);
+        //boolean isSelected = colourCheckBox.isSelected(true);
+
+         */
 
         gridPane.addRow(0, menuBox);
+        gridPane.add(bottomBox, 0, 1);
+        //gridPane.add(colourCheckBox, 0, 2);
+
         Scene scene = new Scene(gridPane, 960, 600);
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
