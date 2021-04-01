90c334f29276b3cc9fed0ce2c16160d6ce44ae5b
Niall <niall.hennessy@ucdconnect.ie>
2021-04-01 21:53:52 +0100

    Dealt mostly with the antialising problem

--- src/sample/ComicPanel.java
+++ src/sample/ComicPanel.java
@@ -58,7 +58,7 @@ public class ComicPanel extends Pane {
         leftCharacterWrapper.setStyle("-fx-border-color: cyan");
         rightCharacterWrapper.setStyle("-fx-border-color: white");
 
-        updateImage("left");
+//        updateImage("left");
     }
 
     public ImageView getRightCharacter() {
@@ -162,7 +162,10 @@ public class ComicPanel extends Pane {
                 for (int x = 0; x < width; x++) {
                     Color color = pixelReader.getColor(x, y);
 
-                    if (color.equals(Color.web("0xf0ff00ff"))) {
+                    if(color.equals(Color.WHITE)){
+                        continue;
+                    }
+                    else if (color.equals(Color.web("0xf0ff00ff"))) {
                         if(!leftFemale)
                             pixelWriter.setColor(x, y, Color.WHITE);
                         else
@@ -175,11 +178,13 @@ public class ComicPanel extends Pane {
                         pixelWriter.setColor(x, y, Color.WHITE);
                     }else if (color.equals(Color.web("#FF0000")) && !leftFemale) {
                         pixelWriter.setColor(x, y, leftCharacterSkin);
-                    }
-                    else if(isOnLine(Color.web("#FF0000"), leftCharacterSkin, color)){
+                    }else if(isOnLine(Color.web("#FF0000"), leftCharacterSkin, color) && !leftFemale){
                         pixelWriter.setColor(x, y, leftCharacterSkin);
-                    }
-                    else {
+                    }else if(isOnLine(Color.web("#ECB4B5"), Color.WHITE, color) && !leftFemale){
+                        pixelWriter.setColor(x, y, Color.WHITE);
+                    }else if(isOnLine(Color.web("0xf0ff00ff"), Color.WHITE, color) && !leftFemale){
+                        pixelWriter.setColor(x, y, Color.WHITE);
+                    }else {
                         pixelWriter.setColor(x, y, color);
                     }
                 }
@@ -289,26 +294,92 @@ public class ComicPanel extends Pane {
 
     public boolean isOnLine(Color p1, Color p2, Color p3)
     {
-        int p1RedColor = Integer.parseInt(p1.toString().substring(2,4), 16);
-        int p1GreenColor = Integer.parseInt(p1.toString().substring(4,6), 16);
-        int p1BlueColor = Integer.parseInt(p1.toString().substring(6,8), 16);
 
-        int p2RedColor = Integer.parseInt(p2.toString().substring(2,4), 16);
-        int p2GreenColor = Integer.parseInt(p2.toString().substring(4,6), 16);
-        int p2BlueColor = Integer.parseInt(p2.toString().substring(6,8), 16);
+        double p1RedColor = Integer.parseInt(p1.toString().substring(2,4), 16);
+        double p1GreenColor = Integer.parseInt(p1.toString().substring(4,6), 16);
+        double p1BlueColor = Integer.parseInt(p1.toString().substring(6,8), 16);
 
-        int p3RedColor = Integer.parseInt(p3.toString().substring(2,4), 16);
-        int p3GreenColor = Integer.parseInt(p3.toString().substring(4,6), 16);
-        int p3BlueColor = Integer.parseInt(p3.toString().substring(6,8), 16);
+        double p2RedColor = Integer.parseInt(p2.toString().substring(2,4), 16);
+        double p2GreenColor = Integer.parseInt(p2.toString().substring(4,6), 16);
+        double p2BlueColor = Integer.parseInt(p2.toString().substring(6,8), 16);
 
+        double p3RedColor = Integer.parseInt(p3.toString().substring(2,4), 16);
+        double p3GreenColor = Integer.parseInt(p3.toString().substring(4,6), 16);
+        double p3BlueColor = Integer.parseInt(p3.toString().substring(6,8), 16);
 
+        boolean red = false;
+        boolean green = false;
+        boolean blue = false;
 
-        if((p3RedColor - p1RedColor) / (p2RedColor - p1RedColor) == (p3GreenColor - p1GreenColor) / (p2GreenColor - p1GreenColor) &&
-                (p3RedColor - p1RedColor) / (p2RedColor - p1RedColor) == (p3BlueColor - p1BlueColor) / (p2BlueColor - p1BlueColor)){
+        double redVal = -1;
+        double greenVal = 0;
+        double blueVal = 1;
+
+        if(p1RedColor - p2RedColor == 0){
+            if(p1RedColor == p3RedColor)
+            {
+                red = true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            redVal = (p3RedColor - p1RedColor) / (p2RedColor - p1RedColor);
+
+            if(redVal >= 0 && redVal <= 1)
+                red = true;
+        }
+
+        if(p1GreenColor - p2GreenColor == 0){
+            if(p1GreenColor == p3GreenColor)
+            {
+                green = true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            greenVal = (p3GreenColor - p1GreenColor) / (p2GreenColor - p1GreenColor);
+
+            if(greenVal >= 0 && greenVal <= 1)
+                green = true;
+        }
+
+        if(p1BlueColor - p2BlueColor == 0){
+            if(p1BlueColor == p3BlueColor)
+            {
+                blue = true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            blueVal = (p3BlueColor - p1BlueColor) / (p2BlueColor - p1BlueColor);
+
+            if(blueVal >= 0 && blueVal <= 1)
+                blue = true;
+        }
+
+        if(red && green && blue) {
+            System.out.println("Found Anti-Aliasling: " + p3.toString());
             return true;
         }
-        else {
+        else if(redVal == greenVal && redVal == blueVal){
+            System.out.println("Found Anti-Aliasling: " + p3.toString());
+            return true;
+        }
+        else{
             return false;
         }
+
     }
 }
