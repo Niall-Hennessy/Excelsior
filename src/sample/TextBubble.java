f32aab23641b3b9c7f5e5d79fb66b4b288306758
Niall <niall.hennessy@ucdconnect.ie>
2021-04-09 17:47:59 +0100

    Fixed some text bubble bugs

--- src/sample/TextBubble.java
+++ src/sample/TextBubble.java
@@ -37,23 +37,30 @@ public class TextBubble extends Pane {
 
     public String parseText(String text, int x){
 
+        text = text.trim();
+
         if(text.length() > x){
             String newText = text.substring(0,x);
+
+            if(newText.contains("\n"))
+                return newText += parseText(text.substring(x), x);
 
             int whiteSpace = -1;
 
             for(int i = 0; i < x; i++)
-                if(newText.charAt(i) == ' ')
+                if (newText.charAt(i) == ' ')
                     whiteSpace = i;
+
 
             if(whiteSpace == -1)
                 newText += "\n";
             else
-                newText = newText.substring(0,whiteSpace) + "\n" + newText.substring(whiteSpace);
+                newText = newText.substring(0,whiteSpace) + "\n" + newText.substring(whiteSpace + 1);
 
             newText += parseText(text.substring(x), x);
             text = newText;
         }
+
         return text;
     }
 }
