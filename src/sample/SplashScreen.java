7963c3af2ecaffed6ed873921f7f50c25f03d71c
Niall <niall.hennessy@ucdconnect.ie>
2021-04-11 18:08:22 +0100

    Added Splash Screen

---
+++ src/sample/SplashScreen.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.application.Preloader;
+import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class SplashScreen extends Preloader {
+    ProgressBar bar;
+    Stage stage;
+
+    private Scene createPreloaderScene() throws FileNotFoundException {
+        bar = new ProgressBar();
+        BorderPane p = new BorderPane();
+        p.setCenter(bar);
+        return new Scene(p, 300, 150);
+    }
+
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.setScene(createPreloaderScene());
+        stage.show();
+    }
+
+    @Override
+    public void handleProgressNotification(ProgressNotification pn) {
+        bar.setProgress(pn.getProgress());
+    }
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification evt) {
+        if (evt.getType() == StateChangeNotification.Type.BEFORE_START) {
+            stage.hide();
+        }
+    }
+}
