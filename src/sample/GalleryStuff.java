85aba8f8f95c49366d27a2489edbd126cdbbbb5b
Peetlebeetle <peterjorio@gmail.com>
2021-04-26 16:40:10 +0100

    Character Gallery desired layout achieved - Next stop: set character images to a nice scale

--- src/sample/GalleryStuff.java
+++ src/sample/GalleryStuff.java
@@ -67,12 +67,12 @@ public class GalleryStuff {
 
         addCharacter.setScene(scene);
         addCharacter.show();
+        addCharacter.setMaxHeight(Screen.getPrimary().getVisualBounds().getHeight());
         addCharacter.setX( (Screen.getPrimary().getVisualBounds().getWidth()/2) - (addCharacter.getWidth()/2) );
         addCharacter.setY( (Screen.getPrimary().getVisualBounds().getHeight()/2) - (addCharacter.getHeight()/2) );
 
         if(addCharacter.getHeight() > Screen.getPrimary().getVisualBounds().getHeight()) {
             addCharacter.setY(0);
-            addCharacter.setMaxHeight(Screen.getPrimary().getVisualBounds().getHeight());
         }
     }
 
