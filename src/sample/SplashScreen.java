6be091471f13d671c8189250be306a82ab0296b3
ChloeK <chloe.keeley@ucdconnect.ie>
2021-04-26 14:55:53 +0100

    preloader wont show before application starts working on fixing this

--- src/sample/SplashScreen.java
+++ src/sample/SplashScreen.java
@@ -1,16 +1,95 @@
 package sample;
 
+import com.sun.javafx.application.LauncherImpl;
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
 import javafx.application.Preloader;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import static com.sun.javafx.font.PrismFontFactory.isEmbedded;
+
+public class SplashScreen extends Preloader {
+    Stage stage;
+    BooleanProperty ready = new SimpleBooleanProperty(false);
+
+    private void longStart() {
+        //simulate long init in background
+        Task task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                int max = 100;
+                for (int i = 1; i <= max; i++) {
+                    Thread.sleep(200);
+                    // Send progress to preloader
+                    notifyPreloader(new ProgressNotification(((double) i)/max));
+                }
+                // After init is ready, the app is ready to be shown
+                // Do this before hiding the preloader stage to prevent the
+                // app from exiting prematurely
+                ready.setValue(Boolean.TRUE);
+
+                notifyPreloader(new StateChangeNotification(
+                        StateChangeNotification.Type.BEFORE_START));
+
+                return null;
+            }
+        };
+        new Thread(task).start();
+    }
+
+    //trying to make preloader go while stage sleeps
+    @Override
+    public void init () throws Exception{
+        int COUNT_LIMIT = 40000;
+        for (int i = 0; i < COUNT_LIMIT; i++) {
+            double progress = (100 * i) / COUNT_LIMIT;
+            LauncherImpl.notifyPreloader(this, new Preloader.ProgressNotification(progress));
+        }
+    }
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        // Initiate simulated long startup sequence
+        longStart();
+
+        stage.setScene(new Scene(new Label("Application started"),
+                400, 400));
+
+        // After the app is ready, show the stage
+        ready.addListener(new ChangeListener<Boolean>(){
+            @Override
+            public void changed(ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) {
+                if (Boolean.TRUE.equals(t1)) {
+                    Platform.runLater(new Runnable() {
+                        public void run() {
+                            stage.show();
+                        }
+                    });
+                }
+            }
+        });;
+    }
+}
+
+
+/*
 public class SplashScreen extends Preloader {
     ProgressBar bar;
     Stage stage;
@@ -34,9 +113,36 @@ public class SplashScreen extends Preloader {
     }
 
     @Override
-    public void handleStateChangeNotification(StateChangeNotification evt) {
-        if (evt.getType() == StateChangeNotification.Type.BEFORE_START) {
-            stage.hide();
+    public void handleStateChangeNotification(StateChangeNotification evt)
+    {
+        if (evt.getType() == StateChangeNotification.Type.BEFORE_START)
+        {
+            //stage.hide();
+            //making the preloader fade out
+            if(isEmbedded && stage.isShowing())
+            {
+                //fade out, hide stage at the end of animation
+                FadeTransition fade = new FadeTransition(Duration.millis(1000), stage.getScene().getRoot());
+                fade.setFromValue(1.0);
+                fade.setToValue(0.0);
+                final Stage s = stage;
+                EventHandler<ActionEvent> acc = new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        s.hide();
+                    }
+                };
+
+                fade.setOnFinished(acc);
+                fade.play();
+            }
+
+            else
+            {
+                stage.hide();
+            }
         }
+
     }
 }
+*/
